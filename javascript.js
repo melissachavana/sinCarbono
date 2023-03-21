629b3ebd2fb235dbc70b78da382f86d1ec6ed258
Melissachavana <chavana.melissa@gmail.com>
2023-03-21 18:22:39 +0100

    contador de 3 intentos

--- javascript.js
+++ javascript.js
@@ -1,28 +1,31 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+let contador = 0;
+
+function calculadora() {
+  if (contador < 3) {
+    HuellaCarbono();
+    contador++;
+  } else {
+    alert("Has utilizado tus 3 intentos del día");
   }
+}
 
+function HuellaCarbono() {
+  alert("valor contador " + contador);
 
+  let consumo_electricidad = document.getElementById("kwh").value;
+  let consumo_gasnatural = document.getElementById("gas").value;
+  let consumo_diesel = document.getElementById("diesel").value;
+  let consumo_agua = document.getElementById("agua").value;
 
+  let huelladecarbono =
+    consumo_agua * 0.2 +
+    consumo_diesel * 2.7 +
+    consumo_gasnatural * 1.8 +
+    consumo_electricidad * 0.42;
 
-  function calculadora() {
-    
-    var consumo_electricidad = document.getElementById("kwh").value;
-    var consumo_gasnatural = document.getElementById("gas").value;
-    var consumo_diesel = document.getElementById("diesel").value;
-    var consumo_agua = document.getElementById("agua").value;
+  alert("Tu huella de carbono es " + huelladecarbono);
+}
 
-
-    let huelladecarbono= consumo_agua*0.2+consumo_diesel*2.7+consumo_gasnatural*1.8+consumo_electricidad*0.42;
-
-    alert("Tu huella de carbono es " + huelladecarbono);
-
-
-    }
+  
 
     $('.carousel').carousel()
