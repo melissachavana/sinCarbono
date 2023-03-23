8ca442277b4e7a56ad437c324e648908464a8760
Melissachavana <chavana.melissa@gmail.com>
2023-03-23 21:54:43 +0100

    prompt, alert, objeto y array en archivo js

--- javascript.js
+++ javascript.js
@@ -1,32 +1,24 @@
 let button = document.getElementById("calcular-btn");
 let contador = 0;
 
-button.addEventListener("click", function(e) {
-	// Increment the click count
-	
-	calculadora();
-  contador++;
-	// If the click count is equal to 3, disable the button
-	if (contador === 3) {
-		button.disabled = true;
-	}
-  e.preventDefault();
 
-});
+const listaEmpresas = [];
 
-function calculadora() {
-	// Only run the HuellaCarbono function if contador is less than 3
-	if (contador < 3) {
-		HuellaCarbono();
-	} else {
-		alert("Has utilizado tus 3 intentos del día");
-	}
+class empresa {
+  constructor(nombre, electricidad, gas, diesel, agua, huella) {
+    this.nombre = nombre.toUpperCase();
+    this.electricidad = Number(electricidad);
+    this.gas = Number(gas);
+    this.diesel = Number(diesel);
+    this.agua = Number(agua);
+    this.huella = Number(huella);
+  }
+};
 
-}
 
 function HuellaCarbono() {
 	
-
+	let nombre_empresa = document.getElementById("usr").value;
 	let consumo_electricidad = document.getElementById("kwh").value;
 	let consumo_gasnatural = document.getElementById("gas").value;
 	let consumo_diesel = document.getElementById("diesel").value;
@@ -38,7 +30,41 @@ function HuellaCarbono() {
 		consumo_gasnatural * 1.8 +
 		consumo_electricidad * 0.42;
 
-	alert("Tu huella de carbono es " + huelladecarbono);
+		alert("Tu huella de carbono es " + huelladecarbono);
+		listaEmpresas.push(new empresa(nombre_empresa,consumo_electricidad, consumo_gasnatural, consumo_diesel, consumo_agua, huelladecarbono));
+	
+}
+
+
+
+button.addEventListener("click", function(e) {
+	// Increment the click count
+	
+	calculadora();
+  alert("Se ha registrado la información de la siguiente empresa " + listaEmpresas[contador].nombre + " y " + listaEmpresas[contador].compensar + " desea compensar emisiones.");
+  contador++;
+
+	// If the click count is equal to 3, disable the button
+	if (contador === 3) {
+		button.disabled = true;
+	}
+  e.preventDefault();
+
+});
+
+
+function calculadora() {
+	// Only run the HuellaCarbono function if contador is less than 3
+	if (contador < 3) {
+		HuellaCarbono();
+		let compensar = prompt("¿Te gustaría compensar emisiones? (sí/no)");
+		listaEmpresas[contador]['compensar']=compensar;
+
+		
+	} else {
+		alert("Has utilizado tus 3 intentos del día");
+	}
+
 }
 
     $('.carousel').carousel()
